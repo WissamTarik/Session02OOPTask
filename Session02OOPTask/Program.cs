04bea3d2148f589583e6cfccbc2ef63485bb415f
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-23 11:41:06 +0300

    Video00

--- Session02OOPTask/Program.cs
+++ Session02OOPTask/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region  Video00 Revision
+            //Struct:is a Value stored in STACK
+            // What can be created in struct?
+            //1.Events
+            //2.Methods
+            //3.Attributes
+            //4.Property
+            //Constructor : has no return type and its name is the name of class or struct
+
+            //OOP:Is a paradigm for writing code
+            //4.Pillers of OOP:
+            /*
+             1.Encapsulation
+             2.Inheritance
+             3.Polymorphism
+             4.Abstraction
+             */
+            //Encapsulation:Separate data from its usage
+            //Class type:Is reference type stored in HEAP
+
+
+            #endregion
         }
     }
 }
