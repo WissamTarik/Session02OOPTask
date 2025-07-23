99ec6844c21cd4f7ffcf41cb5ec4bcbedd5998de
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-23 11:41:40 +0300

    Video01

--- Session02OOPTask/Program.cs
+++ Session02OOPTask/Program.cs
@@ -26,6 +26,30 @@
 
 
             #endregion
+            #region Video01 Class Vs Struct
+            /*Struct
+             1.Is a value type stored in STACK
+             2.Only create object in STACK
+             3.Support encapsulation-Polymorphism (Doesn't support Inheritance and abstraction)
+             4.new keyword select the constructor
+             5.Default constructor initialize the attributes with default values
+             6.life time of struct object is less than class because the object of struct stored in stack frame  
+             7.Access modifer can be used in struct:Private-Internal-Public
+             8.Used with small and simple data
+
+             */
+            /*Class
+             1.Is a reference type stored in HEAP
+             2.Create a reference in stack refer to object in HEAP
+             3.Support the 4 pillars of OOP (Encapsulation-Inheritance-Polymorphism-Abstraction)
+             4. new keyword create the object
+             5.Default constructor create nothing
+             6. life time of class object is larger than struct(removed by garbage collectoe)
+             7.Access modifier can be used in class all of them(Private-Internal-Public-Protected-Protected internal-Private protected)
+             8.Used with large and complex data
+
+             */
+            #endregion
         }
     }
 }
